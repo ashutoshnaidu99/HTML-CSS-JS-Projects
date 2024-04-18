9d3c9f8cea99da2e6b55bcb6a24630e07b603edc
Ashutosh Naidu <136233470+ashutoshnaidu99@users.noreply.github.com>
2024-04-18 12:04:02 +0530

    added weather app

---
+++ Weather App/script.js
@@ -0,0 +1,20 @@
+const apiKey ='4f493a3ae6274838be3148ac026b6f59';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+const searchbox = document.querySelector('.search input');
+const searchbtn = document.querySelector('.search button');
+
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
+    var data = await response.json();
+
+    console.log(data);
+    document.querySelector('.city').innerHTML = data.name;
+    document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°C';
+    document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
+    document.querySelector('.wind').innerHTML = data.wind.speed + "km/hr";
+}
+searchbtn.addEventListener("click" , ()=>{
+    checkWeather(searchbox.value);
+})
+checkWeather();
